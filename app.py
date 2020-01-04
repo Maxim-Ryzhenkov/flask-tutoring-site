b636ff2eb8f6262eff0b32f9cf9b03d5fcf43578
Maksim Ryzhenkov <ryzhenkov.m@gmail.com>
2020-01-05 00:45:09 +0300

    Роуты прописаны полностью

--- app.py
+++ app.py
@@ -4,44 +4,76 @@ import json
 import logging
 from flask import Flask, render_template, request, redirect, url_for
 
-
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 app_dir = os.path.split(os.path.realpath(__file__))[0]
+app_time_format = "%d.%b.%Y - %X"
 
 
 def get_data(file_name: str):
-    """ Чтение данных из файла json
+    """ Прочитать данные из указанного файла json и вернуть их как словарь
     Args:
-        file_name: относительный путь к файлу .json
+        file_name: полный путь к файлу .json
+    Returns:
+        data: dict - содержимое json файла в виде словаря
     """
-    file_name = os.path.abspath(os.path.join(app_dir, file_name))
     if not os.path.exists(file_name):
-        raise OSError
-    logging.debug(f"Файл с данными найден: {file_name}")
+        raise OSError(f"Файл не найден: {file_name}")
     with open(file_name, 'r', encoding='utf8') as json_file:
         data = json.load(json_file)
     return data
 
 
-def add_data_to(file_name: str, new_data: dict):
-    """ Добавление данных в файла json
+def put_data(file_name: str, data: dict):
+    """ Записать данные в файл json. !!! Файл будет перезаписан!
     Args:
-        file_name: относительный путь к файлу .json
-        new_data: слдоварь для добавления в json
+        file_name: полный путь к файлу .json
+        data: слдоварь для записи в json
     """
-    file_name = os.path.abspath(os.path.join(app_dir, file_name))
-    data: dict = {}
-    if os.path.exists(file_name) and os.stat(file_name).st_size != 0:
-        data = get_data(file_name)
-    data.update(new_data)
     with open(file_name, 'w', encoding='utf8') as json_file:
         json.dump(data, json_file, indent=2, ensure_ascii=False)
 
 
+def add_booking(booking_data: dict):
+    """ Добавить новую запись о бронировании в файле booking.json
+        - Если файл не существует, он будет создан и в него будет добавлен ключ "bookings: {}"
+        - Если файл уже есть, то он будет прочитан в словарь.
+        - Новая запись будет добавлена в словарь
+        - После этого словарь будет записан обратно в файл booking.json.
+    """
+    booking_json = os.path.abspath(os.path.join(app_dir, 'booking.json'))
+    if not os.path.exists(booking_json):
+        put_data(booking_json, {'bookings': {}})
+    data = get_data(booking_json)
+    data['bookings'].update(booking_data)
+    put_data(booking_json, data)
+
+
+def add_tutor_request(request_data: dict):
+    """ Добавить новую запись о заявке на подбор репетитора в файле request_tutor.json
+        - Если файл не существует, он будет создан и в него будет добавлен ключ "requests: {}"
+        - Если файл уже есть, то он будет прочитан в словарь.
+        - Новая запись будет добавлена в словарь
+        - После этого словарь будет записан обратно в файл request_tutor.json.
+    """
+    request_tutor_json = os.path.abspath(os.path.join(app_dir, 'request_tutor.json'))
+    if not os.path.exists(request_tutor_json):
+        put_data(request_tutor_json, {'requests': {}})
+    data = get_data(request_tutor_json)
+    data['requests'].update(request_data)
+    put_data(request_tutor_json, data)
+
+
+def get_tutors_data():
+    """ Прочитать данные о репетиторах из файла data.json и вернуть как словарь """
+    tutors_json = os.path.abspath(os.path.join(app_dir, 'data.json'))
+    return get_data(tutors_json)
+
+
 @app.route('/')
 def main():
-    teachers = get_data('data.json')['teachers']
+    """ Главная страница сайта """
+    teachers = get_tutors_data()['teachers']
     return render_template('index.html', teachers=teachers)
 
 
@@ -50,62 +82,66 @@ def goals(goal):
     """ Страница где показываются только репетиторы подходящие под цели обучения
      То есть если у них есть соответствующий тэг.
      """
-    return render_template('goal.html')
+    teachers = get_tutors_data()['teachers']
+    teachers = {k: v for k, v in teachers.items() if goal in v['goals']}
+    goals_list = {"travel": "для путешествий", "study": "для школы", "work": "для работы", "relocate": "для переезда"}
+    return render_template("goal.html", teachers=teachers, goal=goals_list[goal])
 
 
-@app.route('/profiles/<int:id>')
-def profiles(id):
+@app.route('/profile/<int:id>')
+def profile(id: int):
     """ Страница с индивидуальным профилем преподавателя """
-    teachers = get_data('data.json')['teachers']
+    teachers = get_tutors_data()['teachers']
     if str(id) not in teachers.keys():
         return render_template('page404.html')
-    return render_template('profile.html', teacher=teachers[str(id)])
+    return render_template('profile.html', teacher=teachers[str(id)], teacher_id=str(id))
 
 
-@app.route('/search')
-def search():
-    """ Страница поиска """
-    return render_template('search.html')
-
-
-@app.route('/request')
-def request():
+@app.route('/pick', methods=['GET', 'POST'])
+def pick():
     """ Страница заявки на подбор репетитора """
+    if request.method == 'POST':
+        request_time = time.strftime(app_time_format)
+        request_data = {"user name": request.form['user_name'],
+                        "phone": request.form['phone'],
+                        "time": request.form['time'],
+                        "goal": request.form['goal']}
+        add_tutor_request({request_time: request_data})
+        return render_template('pick_confirmed.html', data=request_data)
     return render_template("pick.html")
 
 
 @app.route('/booking/<int:id>', methods=['POST', 'GET'])
 def booking(id):
     """ Страница формы бронирования занятий с репетитором """
-  #  form = BookingForm()
-    teachers = get_data('data.json')['teachers']
+    teachers = get_tutors_data()['teachers']
     if str(id) not in teachers.keys():
         return render_template('page404.html')
     teacher = teachers[str(id)]
+    booking_day = request.args.get("d")
+    booking_time = request.args.get("h")
+    days = {"mon": "понедельник", "tue": "вторник", "wed": "среда", "thu": "четверг", "fri": "пятница", "sat": "суббота", "sun": "воскресенье"}
 
     if request.method == 'POST':
-        username = request.form.get('username')  # запрос к данным формы
-        phone = request.form.get('password')
-        booking_info = {"time": time.localtime(),
-                        "user name": username,
-                        "phone": phone,
-                        "teacher": {"id": id, "name": teacher['name']}}
-        add_data_to('booking.json', booking_info)
-    return render_template('booking.html', teacher=teacher)
+        booking_time = time.strftime(app_time_format)
+        booking_data = {"user name": request.form['user_name'],
+                        "phone": request.form['phone_number'],
+                        "day": days[booking_day],
+                        "time": booking_time,
+                        "tutor": {"id": id, "name": teacher['name']}}
+        add_booking({booking_time: booking_data})
+        return render_template('booking_confirmed.html', booking_data=booking_data)
+    return render_template('booking.html', teacher=teacher, booking_time=booking_time, booking_day=days[booking_day])
 
 
-@app.route('/sent')
-def sent():
-    """ Страница подтверждения бронирования """
-    return render_template("sent.html")
-
-@app.route('/message/<int:id>')
+@app.route('/message/<int:id>', methods=["GET", "POST"])
 def message(id):
     """ Страница формы бронирования занятий с репетитором """
-    teachers = get_data('data.json')['teachers']
+    teachers = get_tutors_data()['teachers']
     if str(id) not in teachers.keys():
         return render_template('page404.html')
-    print(teachers)
+    if request.method == "POST":
+        return render_template('message_confirmed.html')
     return render_template('message.html', teacher=teachers[str(id)])
 
 
@@ -121,7 +157,3 @@ def server_error(e):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    # data = get_data('data.json')
-    # teachers = data['teachers']
-    # print(teachers['1'])
